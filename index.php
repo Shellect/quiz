a12341179dda35c66316743d182d69163747a782
Your name <example@example.com>
2024-06-11 19:18:21 +0300

    fix nginx conf

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 error_reporting(E_ALL);
+session_start();
+
 
 include_once "./views/header.php";
 // Маршрутизатор (router)
@@ -12,6 +14,14 @@ switch ($page) {
         include "login.php";
         break;
     case '1':
+        $email = $_POST['email'] ?? '';
+        if ($email == '') {
+            echo '<p>Отсутствует email</p>';
+            echo '<a href="/">Назад</a>';
+            include_once "./views/footer.php";
+            exit();
+        } 
+        $_SESSION['email'] = $email;
         include 'page1.php';
         break;
     case '2':
